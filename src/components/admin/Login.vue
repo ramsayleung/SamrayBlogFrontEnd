08a5921f11797c78627a6e488903e3036b2176ea
Samray <samrayleung@gmail.com>
2017-02-21 21:33:35 +0800

    add login components

---
+++ src/components/admin/Login.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="vid-container">
+  <video id="Video1" class="bgvid back" autoplay="false" muted="muted" preload="auto" loop>
+      <source src="http://shortcodelic1.manuelmasiacsasi.netdna-cdn.com/themes/geode/wp-content/uploads/2014/04/milky-way-river-1280hd.mp4.mp4" type="video/mp4">
+  </video>
+  <div class="inner-container">
+    <video id="Video2" class="bgvid inner" autoplay="false" muted="muted" preload="auto" loop>
+      <source src="http://shortcodelic1.manuelmasiacsasi.netdna-cdn.com/themes/geode/wp-content/uploads/2014/04/milky-way-river-1280hd.mp4.mp4" type="video/mp4">
+    </video>
+    <div class="box">
+      <h1>Login</h1>
+      <input type="text" placeholder="Username"/>
+      <input type="text" placeholder="Password"/>
+      <button>Login</button>
+    </div>
+  </div>
+</div>
+</template>
+<script>
+require('../../css/login.css')
+require('../../js/login.js')
+</script>
